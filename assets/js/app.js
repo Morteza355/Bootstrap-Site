37590c7a04ba081638e390853fdd1411ee035834
Morteza <mortazapor3@gmail.com>
2022-03-16 14:17:28 +0330

    Fix Some Bugs on login and Register and add Email and Username Regexp and Add a New Article

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,20 @@
 AOS.init();
 let arr = [];
-for (let i = 1; i < 3; i++) {
+for (let i = 1; i < 4; i++) { // you should increase the condition number after adding a new article 
     $.ajax({
         type: "GET",
         url: `assets/articles/article-${i}.html`,
         dataType: "html",
         success: function (response) {
-            arr.push(response);
+            const imgIndex = response.indexOf('img');
+            const jpgIndex = response.indexOf('jpg');
+            const subString = response.substring(imgIndex - 1, jpgIndex+5);
+            let newResponse = response.replace(subString,'');
+            arr.push(newResponse);
             const img = response.replace('..', 'assets');
             const assets = img.indexOf('assets');
-            const sub = img.substring(assets, assets + 26)
+            const jpg = img.indexOf('jpg');
+            const sub = img.substring(assets, jpg+3);
             setTimeout(() => {
                 const string = `<section class="col-lg-3" data-aos="flip-left">
                     <div class="card mb-3 shadow post-card">
@@ -40,16 +45,22 @@ $('#Post-modal').on('show.bs.modal', function (event) {
         url: `assets/articles/article-${recipient}.html`,
         dataType: "html",
         success: function (response) {
-            modal.find('.modal-body #article').html(response);
+            const assets = response.indexOf('img');
+            const jpg = response.indexOf('jpg');
+            const sub = response.substring(assets - 1, jpg+5);
+            let newResponse = response.replace(sub,'');
+            modal.find('.modal-body #article').html(newResponse);
         }
     });
 });
+
+// hightlight nav item on active
 $('.nav-link').click(function (e) {
     $('.nav-link').removeClass('active');
     $(this).addClass('active');
 });
 
-//-----------------------------------------// 
+// Send Input datas to check cookie function on line 86
 const form = document.getElementById('register-form');
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -57,9 +68,9 @@ form.addEventListener('submit', e => {
     const userEmail = e.srcElement[1].value;
     const userPassword = e.srcElement[2].value;
     checkCookieExists(userName, userEmail, userPassword);
-    idGenerate();
 });
 
+// Cookie set and get and check if user exists or not
 function setCoockie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -80,28 +91,25 @@ function getCookie(cname) {
             return cookie.substring(name.length, cookie.length).replace(/\s/g, '');
         }
     }
-    return "";
-}
-
-function RegisterAlert(className, content) {
-    let alertBox = document.getElementById('register-alert-box');
-    alertBox.style.display = 'block';
-    alertBox.innerHTML = content;
-    alertBox.className = className;
+    return null;
 }
 
 function checkCookieExists(username, email, password) {
     let userObj = getCookie('user');
-    if (userObj != '') {
+    if (userObj != null) {
         RegisterAlert('alert alert-danger alert-box-register', 'شما قبلا ثبت نام کرده اید!');
         return;
     } else {
-        if (username.length == 0) {
+        let usernamePattern = /^[a-zA-Z]{5,10}$/;
+        let emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!(usernamePattern.test(username))) {
             RegisterAlert('alert alert-danger alert-box-register', 'نام کاربری خود را به درستی وارد کنید!');
         } else if (password.length == 0) {
             RegisterAlert('alert alert-danger alert-box-register', 'رمز عبور خود را به درستی وارد کنید!');
-        } else if (email.length == 0) {
+        } else if (!(emailPattern.test(email))) {
             RegisterAlert('alert alert-danger alert-box-register', 'ایمیل خود را به درستی وارد کنید!');
+        }else if(document.cookie.includes(username)){
+            RegisterAlert('alert alert-danger alert-box-register', 'این نام کاربری قبلا ثبت شده است !');
         } else {
             userObj = {
                 'username': username,
@@ -114,75 +122,74 @@ function checkCookieExists(username, email, password) {
     }
 }
 
+// ------------------------- // 
+
+// Alerts in Register And Login
+function RegisterAlert(className, content) {
+    let alertBox = document.getElementById('register-alert-box');
+    alertBox.style.display = 'block';
+    alertBox.innerHTML = content;
+    alertBox.className = className;
+}
+
 function loginAlert(className, content) {
     let loginAlert = document.getElementById('login-alert-box');
     loginAlert.style.display = 'block';
     loginAlert.innerHTML = content;
     loginAlert.className = className;
 }
-
+// ------------------------- // 
+let NameLink = document.getElementById('dropdownMenuLink');
+let buttons = document.getElementById('buttons');
+let dropDown = document.getElementById('dropdown');
+// validate input user and password and login the user and if he is not registered he will get an error and set his/her name on localStorage
 function loginUser() {
     const objString = getCookie('user');
-    const realObj = JSON.parse(objString);
-    let loginPassInput = document.getElementById('login-password-input').value;
-    let loginUserInput = document.getElementById('login-username-input').value;
-    if (realObj.username !== loginUserInput) {
-        loginAlert('alert alert-danger alert-box-register', 'نام کاربری وارد شده اشتباه است !');
-    } else if (realObj.password !== loginPassInput) {
-        loginAlert('alert alert-danger alert-box-register', 'رمز عبور وارد شده اشتباه است !');
-    } else {
-        localStorage.setItem('yourName', realObj.username);
-        loginAlert('alert alert-success alert-box-register', 'شما با موفقیت وارد شدید (لطفا چند لحظه صبر کنید)');
-        setTimeout(() => {
+    if (objString != null) {
+        const realObj = JSON.parse(objString);
+        let loginPassInput = document.getElementById('login-password-input').value;
+        let loginUserInput = document.getElementById('login-username-input').value;
+        if (realObj.username !== loginUserInput) {
+            loginAlert('alert alert-danger alert-box-register', 'نام کاربری وارد شده اشتباه است !');
+        } else if (realObj.password !== loginPassInput) {
+            loginAlert('alert alert-danger alert-box-register', 'رمز عبور وارد شده اشتباه است !');
+        } else {
+            localStorage.setItem('yourName', realObj.username);
+            loginAlert('alert alert-success alert-box-register', 'شما با موفقیت وارد شدید (لطفا چند لحظه صبر کنید)');
             location.reload();
-            let userProfile = document.getElementById('user-profile');
-            if (realObj.username != null) {
-                const string = `<section class="user-profile">
-                <span id="user-name" class="fs-5">
-                  <span><i class="bi bi-chevron-down"></i></span>
-                  ${realObj.username}
-                </span>
-                <section class="dropdown" id="dropdown">
-                  <span>اطلاعات حساب کاربری</span>
-                  <span>قفل صفحه</span>
-                  <span id="logout">خروج</span>
-                </section>
-              </section>`
-                userProfile.innerHTML = string;
-            }
-        }, 3000);
+        }
+    } else {
+        loginAlert('alert alert-danger alert-box-register', 'شما هنوز ثبت نام نکردید !');
+        return;
     }
 }
+// ------------------------- // 
+// Call the loginUser Function on form submit
 let loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', e => {
     e.preventDefault();
     loginUser();
 });
+
+// ------------------------- // 
+// get the name on localStorage and create a dynamic html wit user info
 window.addEventListener('load', e => {
     const Name = localStorage.getItem('yourName');
-    let userProfile = document.getElementById('user-profile');
     if (Name != null) {
-        const string = `<section class="user-profile">
-        <span id="user-name" class="fs-5">
-          <span><i class="bi bi-chevron-down"></i></span>
-          ${Name}
-        </span>
-        <section class="dropdown" id="dropdown">
-          <span>اطلاعات حساب کاربری</span>
-          <span>قفل صفحه</span>
-          <span id="logout">خروج</span>
-        </section>
-      </section>`
-        userProfile.innerHTML = string;
+        buttons.style.cssText = 'display:none!important';
+        dropDown.style.display = 'block';
+        NameLink.innerHTML = Name;
+    }else{
+        buttons.style.cssText = 'display:flex!important';
+        dropDown.style.display = 'none';
     }
-    let logoutBtn = document.getElementById('logout');
-    logoutBtn.addEventListener('click',e => {
-        localStorage.removeItem('yourName');
-        location.reload();
-    });
 });
 // ------------------------- // 
-let dropdownActive = document.getElementById('user-profile');
-dropdownActive.addEventListener('click', e => {
-    $('#dropdown').toggle();
-});
+// if logout btn exists just logout the user and remove his name on localStorage
+let logoutBtn = document.getElementById('logout');
+    logoutBtn.addEventListener('click', e => {
+        localStorage.removeItem('yourName');
+        location.reload();
+        dropDown.style.cssText = 'display:none!important';
+    });
+// ------------------------- // 
